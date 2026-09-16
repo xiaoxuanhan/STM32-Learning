28dada804fb183cf2661acf31b197ae32efe7579
xiaoxuanhan <2787246668@qq.com>
2026-09-16 10:31:10 +0800

    Add files via upload

---
+++ 01_LED/main.c
@@ -0,0 +1,18 @@
+#include "sys.h"
+#include "delay.h"
+#include "led.h"
+
+int main(void)
+{
+    HAL_Init();                         /* 初始化HAL库 */
+    stm32_clock_init(RCC_PLL_MUL9); /* 设置时钟, 72Mhz */
+		led_init();
+	
+    while(1)
+    { 
+				led1_on();
+				delay_ms(500);
+				led1_off();
+				delay_ms(500);
+    }
+}
